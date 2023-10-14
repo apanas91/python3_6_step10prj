19c7e80d8bea91b66205a8632a910d8702a07148
sapanasenko <apanas91@gmail.com>
2023-10-15 00:53:52 +0300

    selenium tests

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ class TestShopPage:
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         add_to_basket_btn = browser.find_element(By.CSS_SELECTOR, ".add-to-basket")
-        assert True == add_to_basket_btn.is_displayed(), "Кнопка не отображается"
+        assert add_to_basket_btn.is_displayed(), "Кнопка не отображается"
